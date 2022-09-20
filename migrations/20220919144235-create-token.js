0a5ec0cbcc5054b49839569c04b840e2e3b7751a
Rizki Ashari <80566147+rizkiashari@users.noreply.github.com>
2022-09-20 14:06:09 +0700

    create model token

---
+++ migrations/20220919144235-create-token.js
@@ -0,0 +1,33 @@
+"use strict";
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Tokens", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+      },
+      token: {
+        type: Sequelize.STRING,
+      },
+      expired_at: {
+        type: Sequelize.DATE,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Tokens");
+  },
+};
---
+++ models/token.js
@@ -0,0 +1,26 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Token extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Token.init(
+    {
+      user_id: DataTypes.INTEGER,
+      token: DataTypes.STRING,
+      expired_at: DataTypes.DATE,
+    },
+    {
+      sequelize,
+      modelName: "Token",
+    }
+  );
+  return Token;
+};
