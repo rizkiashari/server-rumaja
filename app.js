22e52faf9c8a5446c71412f4172797b7d9dc72e6
Rizki Ashari <80566147+rizkiashari@users.noreply.github.com>
2022-09-23 22:26:55 +0700

    Add UI Index

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const cors = require("cors");
+const path = require("path");
 const router = require("./src/routes/index.js");
 
 const app = express();
@@ -9,10 +10,17 @@ const env = dotenv.config().parsed;
 
 const PORT = env.PORT || 8881;
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
 app.use(cors());
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 /* API V1 */
 app.use("/api", router);
