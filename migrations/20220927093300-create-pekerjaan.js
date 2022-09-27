2dc3766c4f8b9447e8950abb006d5a914e6f4929
Rizki Ashari <80566147+rizkiashari@users.noreply.github.com>
2022-09-27 16:54:31 +0700

    set name table pekerjaan

--- migrations/20220927093300-create-pekerjaan.js
+++ migrations/20220927093300-create-pekerjaan.js
@@ -1,7 +1,7 @@
 "use strict";
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Pekerjaans", {
+    await queryInterface.createTable("Pekerjaan", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -64,6 +64,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("Pekerjaans");
+    await queryInterface.dropTable("Pekerjaan");
   },
 };
