92562b1be90418de1f038bc30ba934058d8e44b8
Rizki Ashari <80566147+rizkiashari@users.noreply.github.com>
2022-09-27 16:12:44 +0700

    update user penyedia

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,8 @@
 const joi = require("joi");
 const dotenv = require("dotenv");
-const bcrypt = require("bcrypt");
-const uuid = require("uuid");
 const jwt = require("jsonwebtoken");
-const { User, Token } = require("../../models");
-const { errorResponse, successResWithData } = require("../helper/response");
+const { User, Penyedia } = require("../../models");
+const { errorResponse, successResWithData, successRes } = require("../helper/response");
 const { Op } = require("sequelize");
 
 const env = dotenv.config().parsed;
@@ -47,6 +45,81 @@ exports.getAllUser = async (req, res) => {
 
         successResWithData(res, 200, "SUCCESS_GET_ALL_USER", users);
       }
+    });
+  } catch (error) {
+    errorResponse(res, 500, "Internal Server Error");
+  }
+};
+
+exports.updateUserPenyedia = async (req, res) => {
+  try {
+    const headers = req.header("Authorization");
+    const dataPenyedia = req.body;
+    if (!headers) {
+      return errorResponse(res, 401, "Unauthorized");
+    }
+
+    const token = headers.split(" ")[1];
+
+    jwt.verify(token, env.JWT_ACCESS_TOKEN_SECRET, async (err, user) => {
+      if (err) {
+        return errorResponse(res, 403, "TOKEN_EXPIRED");
+      }
+
+      const dataUser = await User.findOne({
+        where: {
+          id: user.id,
+        },
+      });
+
+      if (!dataUser) {
+        return errorResponse(res, 404, "USER_NOT_FOUND");
+      }
+
+      const schema = joi.object({
+        name_user: joi.string().min(3).required(),
+        gender: joi.string().required(),
+        email: joi.string().email().required(),
+        nomor_wa: joi.string().min(10).max(17).required(),
+        alamat_rumah: joi.string().required(),
+        domisili: joi.string().required(),
+        tentang: joi.string().required(),
+        tanggal_lahir: joi.string().required(),
+        photo_profile: joi.string().optional(),
+      });
+
+      const { error } = schema.validate(dataPenyedia);
+
+      if (error) {
+        return errorResponse(res, 400, error.details[0].message);
+      }
+
+      await dataUser.update(
+        {
+          name_user: dataPenyedia.name_user,
+          email: dataPenyedia.email,
+          nomor_wa: dataPenyedia.nomor_wa,
+        },
+        {
+          where: {
+            id: user.id,
+          },
+        }
+      );
+
+      await Penyedia.create({
+        user_id: user.id,
+        domisili: dataPenyedia.domisili,
+        photo_profile: dataPenyedia.photo_profile,
+        header_profile: dataPenyedia.header_profile,
+        gender: dataPenyedia.gender,
+        alamat_rumah: dataPenyedia.alamat_rumah,
+        tanggal_lahir: dataPenyedia.tanggal_lahir,
+        tentang: dataPenyedia.tentang,
+        createdAt: Math.floor(+new Date() / 1000),
+      });
+
+      successRes(res, 200, "SUCCESS_UPDATE_USER_PENYEDIA");
     });
   } catch (error) {
     errorResponse(res, 500, "Internal Server Error");
--- src/routes/user.js
+++ src/routes/user.js
@@ -4,13 +4,16 @@ const {
   findEmailResetPassword,
   changePassword,
 } = require("../controllers/changePasswordController");
-const { getAllUser } = require("../controllers/userController");
+const { getAllUser, updateUserPenyedia } = require("../controllers/userController");
 
 const router = Router();
 
 router.get("/all", getAllUser);
 router.post("/forgot-password", findEmailResetPassword);
 router.post("/change-password", changePassword);
+
+router.post("/update/penyedia", updateUserPenyedia);
+
 router.get("/list-kerja", listBidangKerja);
 
 module.exports = router;
