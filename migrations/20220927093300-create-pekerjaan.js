1dd840bbc8f26126fae1421920ddfedf88347fae
Rizki Ashari <80566147+rizkiashari@users.noreply.github.com>
2022-09-29 11:28:25 +0700

    updeta table pekerjaan

--- migrations/20220927093300-create-pekerjaan.js
+++ migrations/20220927093300-create-pekerjaan.js
@@ -1,7 +1,7 @@
 "use strict";
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Pekerjaan", {
+    await queryInterface.createTable("pekerjaan", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -67,6 +67,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("Pekerjaan");
+    await queryInterface.dropTable("pekerjaan");
   },
 };
