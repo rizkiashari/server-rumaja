492cb5779c7833cf6e698080697eac792d5f944b
Rizki Ashari <80566147+rizkiashari@users.noreply.github.com>
2022-09-29 11:27:15 +0700

    chnage name table pencari

--- migrations/20220919180459-create-pencari.js
+++ migrations/20220919180459-create-pencari.js
@@ -1,7 +1,7 @@
 "use strict";
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Pencari", {
+    await queryInterface.createTable("pencari", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -61,6 +61,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("Pencari");
+    await queryInterface.dropTable("pencari");
   },
 };
