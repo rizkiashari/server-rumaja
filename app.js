b9f336514b98fb13ca16178abc5177eedb3d8a2f
Rizki Ashari <80566147+rizkiashari@users.noreply.github.com>
2022-10-26 22:14:13 +0700

    add access controll allow origin

--- app.js
+++ app.js
@@ -17,6 +17,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "x-access-token, Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.get("/", (req, res) => {
   res.render("index");
