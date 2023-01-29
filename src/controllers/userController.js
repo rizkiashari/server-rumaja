7c8dbe6b8d9152c31941c15aae61cbf63dd92c03
Rizki Ashari <rizkiashari1026@gmail.com>
2023-01-29 21:51:24 +0700

    testing

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -760,7 +760,7 @@ exports.pencariByBidangKerja = async (req, res) => {
       });
     } else {
       if (jenis_kelamin && min_usia !== 0 && max_usia !== 0) {
-        console.log("2");
+        console.log("2 Aja");
         const totalRows = await Pencari.count({
           where: {
             [Op.and]: [
