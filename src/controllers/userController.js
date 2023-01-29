8258d0683d53a7a013d74e7f01fe59822214ec85
Rizki Ashari <rizkiashari1026@gmail.com>
2023-01-29 21:42:06 +0700

    fix bug filter user pencari

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -758,7 +758,7 @@ exports.pencariByBidangKerja = async (req, res) => {
         totalRows,
       });
     } else {
-      if (jenis_kelamin && min_usia && max_usia) {
+      if (jenis_kelamin && min_usia !== 0 && max_usia !== 0) {
         const totalRows = await Pencari.count({
           where: {
             [Op.and]: [
