3d51e287b5c78de00d90e7a463c63b3e93a0a9fe
Rizki Ashari <rizkiashari1026@gmail.com>
2023-02-19 11:52:51 +0700

    add node mailer

--- src/controllers/changePasswordController.js
+++ src/controllers/changePasswordController.js
@@ -1,6 +1,7 @@
 const { User } = require("../../models");
 const { errorResponse, successRes, successResWithData } = require("../helper/response");
 const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
 const dotenv = require("dotenv");
 const joi = require("joi");
 const bcrypt = require("bcrypt");
@@ -27,11 +28,57 @@ exports.findEmailResetPassword = async (req, res) => {
       return errorResponse(res, 400, "EMAIL_NOT_FOUND");
     }
 
-    const resetPassword = jwt.sign({ email }, env.JWT_RESET_PASSWORD_SECRET);
+    const transporter = nodemailer.createTransport({
+      port: 465,
+      host: "smtp.gmail.com",
+      auth: {
+        user: env.EMAIL,
+        pass: env.PASSWORD,
+      },
+      secure: true,
+    });
 
-    await user.update({ resetPassword });
+    const rekomendasiPass = Math.random().toString(36).slice(-8);
 
-    successResWithData(res, 200, "SUCCESS_FIND_EMAIL", { link: resetPassword });
+    const mailData = {
+      from: env.EMAIL,
+      to: email,
+      subject: "No reply - Permintaan Kata Sandi Baru untuk Rumaja",
+      html: `
+      <p>Kepada Pengguna yang Terhormat,</p>
+      <br>
+      <p>Kami menerima permintaan dari Anda untuk membuat kata sandi baru karena Anda telah lupa kata sandi Anda. Jika Anda tidak merasa melakukan permintaan, dimohon untuk mengabaikan email ini.</p>
+      <br>
+      <p>Berikut adalah kata sandi baru Anda: ${rekomendasiPass}. Silahkan masukan kata sandi berikut pada halaman Kata Sandi Baru, untuk melakuakn konfirmasi permintaan kata sandi baru Anda.</p>
+      <br>
+      <p>Kami sangat menyarankan Anda untuk mengganti kata sandi ini setelah Anda berhasil login dengan mengikuti langkah berikut:</p>
+      <br>
+      <p>1. Buka halaman Profil Anda </p>
+      <p>2. Tekan ikon Pengaturan</p>
+      <p>3. Pilih "Ubah kata sandi"</p>
+      <br>
+      <br>
+      <p> Salam Hormat,</p>
+      <br>
+      <p>Rumaja Dev</p>
+      `,
+    };
+
+    transporter.sendMail(mailData, async function (err, info) {
+      if (err) {
+        console.log(err);
+        errorResponse(res, 500, `Error sending email: ${err}`);
+      } else {
+        const resetPassword = jwt.sign(
+          { rekomendasiPass },
+          env.JWT_RESET_PASSWORD_SECRET
+        );
+
+        await user.update({ resetPassword });
+
+        successResWithData(res, 200, "SUCCESS_FIND_EMAIL", { link: resetPassword });
+      }
+    });
   } catch (error) {
     errorResponse(res, 500, "Internal Server Error");
   }
